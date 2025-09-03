c6a96e8eaa17dcccfe1206a938d94d71f98a037c
emmanuel <emmanuelokwara001@gmail.com>
2025-09-03 09:13:38 +0200

    added required pages

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,9 +1,11 @@
+import { Helix_ } from "@/util/components/loader/loader";
+
 export default async function Page() {
   //console.log('session.user:', session?.user as IUser);
   return (
     <>
-      <div className=" bg-gradient-to-tr from-l_f_s to-l_f_f min-h-[95.8svh] flex justify-center items-center">
-        <div className="h-full flex justify-center items-center text-xl">DASHBOARD COMING Soon!</div>
+      <div className="  flex-col gap-2 bg-gradient-to-tr from-l_f_s to-l_f_f min-h-[95.8svh] flex justify-center items-center">
+        <Helix_/>
       </div>
     </>
   );
